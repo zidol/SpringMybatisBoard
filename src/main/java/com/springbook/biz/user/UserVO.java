8bf63e8ebe0840de782b35ea0a9339ba91265db4
zidol <zidol@218.38.137.27>
2019-06-26 22:06:37 +0900

    oauth 추가 컬럼 vo등록

--- src/main/java/com/springbook/biz/user/UserVO.java
+++ src/main/java/com/springbook/biz/user/UserVO.java
@@ -2,6 +2,9 @@ package com.springbook.biz.user;
 
 import java.sql.Date;
 
+import lombok.Data;
+
+@Data
 public class UserVO {
 	private String id;
 	private String password;
@@ -11,75 +14,9 @@ public class UserVO {
 	private String hp;
 	private String address;
 	private Date reg_date;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
-	public Date getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-
-	public String getHp() {
-		return hp;
-	}
-
-	public void setHp(String hp) {
-		this.hp = hp;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public Date getReg_date() {
-		return reg_date;
-	}
-
-	public void setReg_date(Date reg_date) {
-		this.reg_date = reg_date;
-	}
-
-	@Override
-	public String toString() {
-		return "UserVO [id=" + id + ", password=" + password + ", name=" + name + ", sex=" + sex + ", birthday="
-				+ birthday + ", hp=" + hp + ", address=" + address + ", reg_date=" + reg_date + "]";
-	}
+	private String email;
+	private String googleid;
+	private String naverid;
+	private String nickname;
 
 }
