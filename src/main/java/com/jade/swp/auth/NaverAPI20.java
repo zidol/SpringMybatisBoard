b557c04247336c377c719fbe2d27d99dd086c487
zidol <zidol@192.168.5.19>
2019-07-01 23:26:12 +0900

    페이스북 라이브러리

--- src/main/java/com/jade/swp/auth/NaverAPI20.java
+++ src/main/java/com/jade/swp/auth/NaverAPI20.java
@@ -3,10 +3,12 @@ package com.jade.swp.auth;
 import com.github.scribejava.core.builder.api.DefaultApi20;
 
 public class NaverAPI20 extends DefaultApi20 implements SnsUrls {
+	//싱글톤 패턴
 	private NaverAPI20() {
 	}
 	
 	private static class InstanceHolder {
+		//final 타입이 한번 생성 후 수정 불가 
 		private static final NaverAPI20 INSTANCE = new NaverAPI20();
 	}
 	
